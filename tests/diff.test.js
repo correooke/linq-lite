778c5aa98d5343639c952663b8071b990eb96069
HSNBDES33\eocariz <eocariz@hasar.com>
2019-01-27 16:04:32 -0300

    limpieza

--- tests/diff.test.js
+++
@@ -1,5 +0,0 @@
-const diff = require('../src/diff');
-
-test('diff(a,b) should be a-b', () => {
-  expect(diff(3, 1)).toBe(2);
-});
--- tests/sum.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('../src/sum');
-
-test('sum of 1 and 2 is 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
